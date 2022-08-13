4e362ee095fba12241db16d3a078984d9c7e121f
Floriane <floriane_le_floch@hotmail.fr>
2022-08-13 14:23:43 +0200

    add rainbowKit and its connect wallet button

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,39 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from "wagmi/providers/public";
+
+
+const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [alchemyProvider({ apiKey: alchemyKey })],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: "web3rsvp",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
